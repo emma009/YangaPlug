a1300ac496cc80eb7b2ba3b11400af55cf58fca0
Morgan Nwaiku <morgannwaiku@gmail.com>
2025-01-10 15:54:58 +0000

    Update MapsActivity.java

--- app/src/main/java/com/project/myappfleetapplication/MapsActivity.java
+++ app/src/main/java/com/project/myappfleetapplication/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Lagos = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(Lagos).title("Marker in Lagos"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Lagos));
     }
 }
